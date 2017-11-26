73710c1ca4266c341cd1760b4689729edcac2289
jazzz6992 <haramambooru@gmail.com>
2017-11-27 01:55:49 +0300

    task1 done

--- HomeWork5/src/task1/Main.java
+++ HomeWork5/src/task1/Main.java
@@ -7,11 +7,15 @@ public class Main {
         gadgets[1] = new HiFi(true, HiFi.Source.CD);
         gadgets[2] = new TVSet(false);
         for (Gadget gadget : gadgets) {
+            System.out.println("****************");
             gadget.setOn(true);
+            System.out.println("****************");
         }
         Thread.sleep(3000);
         for (Gadget gadget : gadgets) {
+            System.out.println("****************");
             gadget.setOn(false);
+            System.out.println("****************");
         }
     }
 }
