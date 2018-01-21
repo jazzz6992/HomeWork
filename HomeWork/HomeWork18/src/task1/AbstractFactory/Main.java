d0a3db34b137df95e911cf6f40922c026c6aae22
jazzz6992 <haramambooru@gmail.com>
2018-01-21 20:48:46 +0300

    done

--- HomeWork/HomeWork18/src/task1/AbstractFactory/Main.java
+++ HomeWork/HomeWork18/src/task1/AbstractFactory/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println(furnitureFactory.createChair("wood", 1, 1, 1).getClass().getSimpleName());
         System.out.println(furnitureFactory.createTable("wood", 1, 2, 2).getClass().getSimpleName());
         furnitureFactory = new ModernFurnitureFactory();
+        System.out.println();
         System.out.println(furnitureFactory.createChair("metal", 1, 1, 1).getClass().getSimpleName());
         System.out.println(furnitureFactory.createTable("metal", 1, 2, 2).getClass().getSimpleName());
     }
