9299c065b689cd5ffc51bf3b2328efedaa2656a5
jazzz6992 <haramambooru@gmail.com>
2018-01-22 14:08:31 +0300

    done

--- ClassWork/src/l19/MyCollection.java
+++ ClassWork/src/l19/MyCollection.java
@@ -3,7 +3,6 @@ package l19;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
-import java.util.function.Consumer;
 
 public class MyCollection<Department, Staff> implements MyCollectionContract<Department, Staff> {
 
@@ -59,26 +58,68 @@ public class MyCollection<Department, Staff> implements MyCollectionContract<Dep
     @NotNull
     @Override
     public Iterator<Staff> iterator() {
-        return new Iterator<Staff>() {
-            @Override
-            public boolean hasNext() {
+        return new MyCollectionIterator();
+    }
+
+    public class MyCollectionIterator implements Iterator<Staff> {
+        private Iterator<Map.Entry<Department, List<Staff>>> entryIterator;
+        private Iterator<Staff> staffIterator = null;
+        private Map.Entry<Department, List<Staff>> currentEntry = null;
+
+        public MyCollectionIterator() {
+            entryIterator = staff.entrySet().iterator();
+        }
+
+        @Override
+        public boolean hasNext() {
+            if (staffIterator != null) {
+                if (staffIterator.hasNext()) {
+                    return true;
+                }
+            }
+            if (entryIterator.hasNext()) {
+                currentEntry = entryIterator.next();
+                staffIterator = currentEntry.getValue().iterator();
+                return hasNext();
+            } else {
                 return false;
             }
+        }
 
-            @Override
-            public Staff next() {
-                return null;
+        @Override
+        public Staff next() {
+            return staffIterator.next();
+        }
+
+        @Override
+        public void remove() {
+            staffIterator.remove();
+        }
+    }
+
+    public static void main(String[] args) {
+        MyCollection<String, Integer> myCollection = new MyCollection<>();
+        for (int i = 0; i < 4; i++) {
+            String dep = "dep" + i + 1;
+            myCollection.addDepartment(dep);
+            for (int j = 0; j < 4; j++) {
+                myCollection.addStaff(Integer.valueOf(String.valueOf(i + 1) + String.valueOf(j + 1)), dep);
             }
-        };
-    }
+        }
+        Iterator<Integer> iterator = myCollection.iterator();
+        if (iterator.hasNext()) {
+            System.out.println(iterator.next());
+            iterator.remove();
+        }
+        if (iterator.hasNext()) {
+            System.out.println(iterator.next());
+            iterator.remove();
+        }
 
-    @Override
-    public void forEach(Consumer<? super Staff> action) {
-
-    }
-
-    @Override
-    public Spliterator<Staff> spliterator() {
-        return null;
+        System.out.println();
+        for (Integer i :
+                myCollection) {
+            System.out.println(i);
+        }
     }
 }
